5c335f971f9d250e4cbf1f8bb82f81b8f78caa13
libunamari <marialibunao@gmail.com>
2017-10-18 11:26:30 +1300

    pylint

--- cpu_ram_monitor.py
+++ cpu_ram_monitor.py
@@ -1,47 +1,47 @@
-import psutil
+"""A HTTP server that sends back its own utilisation stats"""
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-from SocketServer import ThreadingMixIn
-from threading import Thread, Timer
-import requests
+from threading import Timer
 import json
-import time
 import decimal
 import os
+import psutil
+import requests
 
 PORT = 12345
-paths = ["/start-monitoring", "/end-monitoring"]
+PATHS = ["/start-monitoring", "/end-monitoring"]
 monitoring_active = False
 multiple_harddrives = False
 
 class HTTPHandler(BaseHTTPRequestHandler):
-    
-    """
-    Set up HTTP headers
-    """
-    def _set_headers(self,code,ctype):
+
+    def _set_headers(self, code, ctype):
+        """
+        Set up HTTP headers
+        """
         self.send_response(code)
         self.send_header('Content-type', ctype)
         self.end_headers()
-    
-    """
-    Handle a HTTP POST message
-    """
+
     def do_POST(self):
+        """
+        Handle a HTTP POST message
+        """
         global monitoring_active, multiple_harddrives
-        
+
         #check path
-        if not self.path in paths:
+        if not self.path in PATHS:
             self._set_headers(404, 'text/html')
-            self.wfile.write('Path not found\n')     
+            self.wfile.write('Path not found\n')
             return
 
-        if self.path == paths[0]:
+        if self.path == PATHS[0]:
             #dont do anything if it's already monitoring
             if monitoring_active:
                 self._set_headers(405, 'text/html')
                 self.wfile.write('Monitoring is already active\n')
-                return     
-            
+                return
+
             multiple_harddrives = os.path.exists("/home/hduser/harddrive2")
             monitoring_active = True
             self.monitor(self.client_address[0])
@@ -50,21 +50,21 @@ class HTTPHandler(BaseHTTPRequestHandler):
             #stop monitoring
             monitoring_active = False
 
-        self._set_headers(200,'text/html')
+        self._set_headers(200, 'text/html')
         self.wfile.write('Success')
 
     def log_message(self, format, *args):
         return
 
-    """
-    Monitor own stats every 1 second.
-    """
     def monitor(self, main_server):
+        """
+        Monitor own stats every 1 second.
+        """
         if not monitoring_active:
             return
 
         #create a thread to start after 1 second
-        Timer(1.0, self.monitor,args=(main_server,)).start()
+        Timer(1.0, self.monitor, args=(main_server,)).start()
 
         #get CPU, RAM, and harddisk stats
         stats = {}
@@ -74,13 +74,13 @@ class HTTPHandler(BaseHTTPRequestHandler):
             hd1 = psutil.disk_usage("/home/hduser/harddrive1")
             hd2 = psutil.disk_usage("/home/hduser/harddrive2")
             usage = ((hd1.used + hd2.used)/decimal.Decimal(hd1.total + hd2.total))*100
-            stats["disk_usage"] = round(usage,1)
+            stats["disk_usage"] = round(usage, 1)
         else:
             stats["disk_usage"] = psutil.disk_usage("/home/hduser/harddrive").percent
         #send to main server
-        url = "http://{}:{}/push-stats".format(main_server,PORT)
+        url = "http://{}:{}/push-stats".format(main_server, PORT)
         headers = {'content-type': 'application/json'}
-        requests.post(url,data=json.dumps(stats), headers=headers)
+        requests.post(url, data=json.dumps(stats), headers=headers)
 
 
 #start up the server
--- cpu_ram_monitor_main.py
+++ cpu_ram_monitor_main.py
@@ -1,33 +1,39 @@
-import psutil
+"""
+A Multithreaded HTTP server that collects utilisation stats from
+the Hadoop nodes specified in $HADOOP_HOME/etc/hadoop/slaves
+"""
+
 import os
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from SocketServer import ThreadingMixIn
 from threading import Thread, Timer
-import requests
 import json
 import time
 import cgi
 import socket
 import fcntl
+import requests
+import psutil
 
 PORT = 12345
 monitoring_active = False
-paths = ["/start-monitoring", "/end-monitoring", "/push-stats"]
+PATHS = ["/start-monitoring", "/end-monitoring", "/push-stats"]
 directory = ""
+
 class HTTPHandler(BaseHTTPRequestHandler):
 
-    """
-    Set up HTTP headers
-    """
-    def _set_headers(self,code,ctype):
+    def _set_headers(self, code, ctype):
+        """
+        Set up HTTP headers
+        """
         self.send_response(code)
         self.send_header('Content-type', ctype)
         self.end_headers()
-    
+
     def _read_data(self):
         # check if received data is JSON
-        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-         if ctype != 'application/json':
+        ctype, _ = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype != 'application/json':
             self._set_headers(400, 'text/html')
             self.wfile.write('POST data is not a JSON object\n')
             return None
@@ -36,20 +42,19 @@ class HTTPHandler(BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length)
         return json.loads(post_data)
 
-
-    """
-    Handle a HTTP POST message
-    """
     def do_POST(self):
+        """
+        Handle a HTTP POST message
+        """
         global monitoring_active, directory
         #check path
-        if not self.path in paths:
+        if not self.path in PATHS:
             self._set_headers(404, 'text/html')
-            self.wfile.write('Path not found\n')     
+            self.wfile.write('Path not found\n')
             return
 
-        if self.path == paths[0]:
-            json_data = _read_data()
+        if self.path == PATHS[0]:
+            json_data = self._read_data()
             if json_data is None:
                 return
             directory = json_data['directory']
@@ -60,56 +65,44 @@ class HTTPHandler(BaseHTTPRequestHandler):
                     f.write('name,tags,time,value\n')
 
             #start monitoring stats
-            self.modify_monitoring(paths[0])
+            self.modify_monitoring(PATHS[0])
             monitoring_active = True
             self.monitor()
-        elif self.path == paths[1]:
+        elif self.path == PATHS[1]:
             #stop monitoring
-            self.modify_monitoring(paths[1])
+            self.modify_monitoring(PATHS[1])
             monitoring_active = False
         else:
-            #receiving stats from a node
-            #so check if received data is JSON
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-
-            if ctype != 'application/json':
-                self._set_headers(400, 'text/html')
-                self.wfile.write('POST data is not a JSON object.\n')
-                return
-
             #parse the JSON object
-            json_data = _read_data()
+            json_data = self._read_data()
             if json_data is None:
                 return
             #get the hostname from the IP address of sender
             host = socket.gethostbyaddr(self.client_address[0])
-            self.write_stats(host[0],json_data)
-        
+            self.write_stats(host[0], json_data)
+
         #got here so code hasn't returned from errors
-        self._set_headers(200,'text/html')
+        self._set_headers(200, 'text/html')
         self.wfile.write('Success')
 
-    """
-    Write the stats to a measurement in influxdb.
-    The content param should be a dict containing the stats.
-    It will be saved to the mlab database in influxdb.
-    Each measurement title is the keys in the dict, and the 
-    value is the value associated with the key. The measurement
-    will also have a tag with the hostname of where the stats 
-    came from.
-    """
     def write_stats(self, client, content):
+        """
+        Writes the stats into a file in the given directory.
+        The time is a unix timestamp in nanoseconds.
+        """
         for key, value in content.items():
-            line = '{},host={},{:19d},{}\n'.format(key,client,int(time.time()*1000000000),value)
+            line = '{},host={},{:19d},{}\n'.format(key, client, int(time.time()*1000000000), value)
             with open(os.path.join(directory, 'indv_ports_' + key + '.csv'), 'a') as f:
+                #obtain lock on file and write
                 fcntl.flock(f, fcntl.LOCK_EX)
                 f.write(line)
                 fcntl.flock(f, fcntl.LOCK_UN)
 
-    """
-    Either start or end monitoring. This depends on the path
-    """
+
     def modify_monitoring(self, path):
+        """
+        Either start or end monitoring. This depends on the path
+        """
         slaves_file = os.path.join(os.environ["HADOOP_HOME"], "etc", "hadoop", "slaves")
         slaves = []
         # get the slave hostnames
@@ -117,28 +110,29 @@ class HTTPHandler(BaseHTTPRequestHandler):
             for line in f:
                 line = line.strip()
                 #don't get this node's name if it is in the slaves file
-                if line != os.uname()[1]: 
+                if line != os.uname()[1]:
                     slaves.append(line)
 
         #send a request to all the slaves using threads so the time between
-        #each request to the nodes is minimised. 
+        #each request to the nodes is minimised.
         for slave in slaves:
-            t = Thread(target=self.send_request, args=(slave,path,))
+            t = Thread(target=self.send_request, args=(slave, path,))
             t.start()
-    
+
     def log_message(self, format, *args):
         return
-    """
-    Send a HTTP POST request to a slave 
-    """
+
     def send_request(self, slave, path):
-        url = "http://{}:{}{}".format(slave,PORT,path)
+        """
+        Send a HTTP POST request to a slave
+        """
+        url = "http://{}:{}{}".format(slave, PORT, path)
         requests.post(url)
-    
-    """
-    Monitor own stats every 1 second.
-    """
+
     def monitor(self):
+        """
+        Monitor own stats every 1 second.
+        """
         if not monitoring_active:
             return
         #create a thread to start after 1 second
@@ -149,7 +143,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
         stats["cpu_percent"] = psutil.cpu_percent()
         stats["virtual_memory"] = psutil.virtual_memory().percent
         stats["disk_usage"] = psutil.disk_usage("/").percent
-        self.write_stats(os.uname()[1],stats)
+        self.write_stats(os.uname()[1], stats)
 
 #use a threaded server so that it can handle multiple requests at once
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
