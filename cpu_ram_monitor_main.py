d6c67106ee1b9997aaa2afaa5a72450b7fc8818a
libunamari <marialibunao@gmail.com>
2017-10-17 16:25:28 +1300

    Write to a file instead of InfluxDB. Use Timer to trigger a stats write instead of time.sleep()

--- cpu_ram_monitor_main.py
+++ cpu_ram_monitor_main.py
@@ -8,11 +8,12 @@ import json
 import time
 import cgi
 import socket
+import fcntl
 
 PORT = 12345
-paths = ["/start-monitoring", "/end-monitoring", "/push-stats"]
 monitoring_active = False
-
+paths = ["/start-monitoring", "/end-monitoring", "/push-stats"]
+directory = ""
 class HTTPHandler(BaseHTTPRequestHandler):
 
     """
@@ -23,12 +24,24 @@ class HTTPHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', ctype)
         self.end_headers()
     
+    def _read_data(self):
+        # check if received data is JSON
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+         if ctype != 'application/json':
+            self._set_headers(400, 'text/html')
+            self.wfile.write('POST data is not a JSON object\n')
+            return None
+
+        content_length = int(self.headers.getheader('content-length'))
+        post_data = self.rfile.read(content_length)
+        return json.loads(post_data)
+
+
     """
     Handle a HTTP POST message
     """
     def do_POST(self):
-        global monitoring_active
-
+        global monitoring_active, directory
         #check path
         if not self.path in paths:
             self._set_headers(404, 'text/html')
@@ -36,12 +49,20 @@ class HTTPHandler(BaseHTTPRequestHandler):
             return
 
         if self.path == paths[0]:
+            json_data = _read_data()
+            if json_data is None:
+                return
+            directory = json_data['directory']
+
+            to_monitor = ["cpu_percent", "virtual_memory", "disk_usage"]
+            for stm in to_monitor:
+                with open(os.path.join(directory, 'indv_ports_' + stm + '.csv'), 'w') as f:
+                    f.write('name,tags,time,value\n')
+
             #start monitoring stats
             self.modify_monitoring(paths[0])
             monitoring_active = True
-            #start up thread to monitor this node's stats
-            t = Thread(target=self.monitor, args=())
-            t.start()
+            self.monitor()
         elif self.path == paths[1]:
             #stop monitoring
             self.modify_monitoring(paths[1])
@@ -57,11 +78,12 @@ class HTTPHandler(BaseHTTPRequestHandler):
                 return
 
             #parse the JSON object
-            content_length = int(self.headers.getheader('content-length'))
-            post_data = self.rfile.read(content_length)
+            json_data = _read_data()
+            if json_data is None:
+                return
             #get the hostname from the IP address of sender
             host = socket.gethostbyaddr(self.client_address[0])
-            self.write_stats(host[0],json.loads(post_data))
+            self.write_stats(host[0],json_data)
         
         #got here so code hasn't returned from errors
         self._set_headers(200,'text/html')
@@ -78,10 +100,11 @@ class HTTPHandler(BaseHTTPRequestHandler):
     """
     def write_stats(self, client, content):
         for key, value in content.items():
-            data = "{},host={} value={}".format(key,client,value)
-            influx_url = "http://localhost:8086/write?db=mlab"
-            r = requests.post(influx_url, data=data)
-
+            line = '{},host={},{:19d},{}\n'.format(key,client,int(time.time()*1000000000),value)
+            with open(os.path.join(directory, 'indv_ports_' + key + '.csv'), 'a') as f:
+                fcntl.flock(f, fcntl.LOCK_EX)
+                f.write(line)
+                fcntl.flock(f, fcntl.LOCK_UN)
 
     """
     Either start or end monitoring. This depends on the path
@@ -114,22 +137,17 @@ class HTTPHandler(BaseHTTPRequestHandler):
     Monitor own stats every 1 second.
     """
     def monitor(self):
-        start_time = time.time()
-        i = 1
-        while(monitoring_active):
-            #get CPU, RAM, and harddisk stats
-            stats = {}
-            stats["cpu_percent"] = psutil.cpu_percent()
-            stats["virtual_memory"] = psutil.virtual_memory().percent
-            stats["disk_usage"] = psutil.disk_usage("/").percent
-            self.write_stats(os.uname()[1],stats)
+        if not monitoring_active:
+            return
+        #create a thread to start after 1 second
+        Timer(1.0, self.monitor).start()
 
-            time_to_sleep = start_time + i - time.time()
-
-            if time_to_sleep > 0:
-                time.sleep(time_to_sleep)
-
-            i += 1
+        #get CPU, RAM, and harddisk stats
+        stats = {}
+        stats["cpu_percent"] = psutil.cpu_percent()
+        stats["virtual_memory"] = psutil.virtual_memory().percent
+        stats["disk_usage"] = psutil.disk_usage("/").percent
+        self.write_stats(os.uname()[1],stats)
 
 #use a threaded server so that it can handle multiple requests at once
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
