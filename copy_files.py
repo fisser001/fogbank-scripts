72f0c409c4c31449e7bb4ffb66a44af5a71f0d53
libunamari <marialibunao@gmail.com>
2017-10-19 10:51:00 +1300

    rename file to change dashes to underscores

--- copy_files.py
+++ copy_files.py
@@ -3,11 +3,6 @@ file_list =["core-site.xml","mapred-site.xml","hdfs-site.xml","yarn-site.xml"]
 PATH_TO_FILE = "/usr/local/hadoop/etc/hadoop/"
 
 for f in file_list:
-	for i in range(1,11):
+	for i in range(11,20):
 		cmd = "scp " + PATH_TO_FILE + "slave_conf/"  + f + " slave" + str(i) + ":" + PATH_TO_FILE
 		os.system(cmd)
-'''
-for j in range(1,11):
-    cmd = "scp /usr/local/hive/conf/hive-site.xml slave" + str(j) + ":/usr/local/hive/conf/hive-site.xml"
-    os.system(cmd)
-'''
