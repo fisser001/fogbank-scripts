9013d9807beeffb2779de1fb3506588177a2ec6e
libunamari <marialibunao@gmail.com>
2017-10-19 10:39:45 +1300

    Added script that adds hosts to the node's /etc/hosts and changes the hostname

---
+++ modify_etc_host.py
@@ -0,0 +1,34 @@
+import paramiko
+
+def run_sudo_command(client, cmd):
+    stdin, stdout, stderr = client.exec_command(cmd, get_pty=True)
+    stdin.write('hduser\n')
+    stdin.flush()
+    data = stdout.read()
+
+new_contents = '127.0.0.1\tlocalhost\n#127.0.1.1\thduser\n\n'\
+               '# The following lines are desirable for IPv6 capable hosts\n' \
+               '::1\tip6-localhost ip6-loopback\nfe00::0\tip6-localnet\n' \
+               'ff00::0\tip6-mcastprefix\nff02::1\tip6-allnodes\n'\
+               'ff02::2\tip6-allrouters\nff02::3\tip6-allhosts\n\n'
+
+with open('node_ip_hostname.txt', 'r') as f:
+    new_contents += f.read()
+
+for i in range(11,20):
+    print('Modifying slave'+str(i))
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    hostname = 'slave' + str(i) 
+    ssh.connect(hostname = hostname, username = 'hduser', look_for_keys=True)
+    
+    cmd = 'echo "{}\n" | sudo tee /etc/hosts'.format(new_contents)
+    run_sudo_command(ssh, cmd)
+
+    cmd = 'sudo hostname ' + hostname
+    run_sudo_command(ssh, cmd)
+
+    cmd = 'echo "{}" | sudo tee /etc/hostname'.format(hostname)
+    run_sudo_command(ssh, cmd)
+
+
