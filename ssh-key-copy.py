4cd92ff03b6e513d45a2dfcab92f5508049d9e98
libunamari <marialibunao@gmail.com>
2017-10-19 10:44:53 +1300

    Changed ssh copy id script to not prompt for user input

--- ssh-key-copy.py
+++
@@ -1,4 +0,0 @@
-import os
-for i in range(1,11):
-	cmd = "ssh-copy-id -i /home/hduser/.ssh/id_rsa.pub hduser@slave" + str(i)
-	os.system(cmd)
---
+++ ssh_key_copy.sh
@@ -0,0 +1,4 @@
+for i in {11..19}
+    do
+        sshpass -p 'hduser' ssh-copy-id -i /home/hduser/.ssh/id_rsa.pub -o StrictHostKeyChecking=no hduser@slave${i}
+    done
