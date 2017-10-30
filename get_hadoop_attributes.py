3edfb05117bf53f700fb05a9467ec5c91f3cf7b9
libunamari <marialibunao@gmail.com>
2017-10-31 12:09:45 +1300

    Parsing the paths to another function

--- get_hadoop_attributes.py
+++ get_hadoop_attributes.py
@@ -1,21 +1,25 @@
 import os
 import subprocess
 
+def parse_paths(paths):
+    parsed_path = []
+    for path in paths.split(","):
+        path = path.strip()
+        if path.startswith("file://"):
+            path = path[7:]
+        parsed_path.append(path)
+
+    return parsed_path
+
 def get_hadoop_dir(attribute):
     hadoop_path = os.getenv("HADOOP_HOME", "/usr/local/hadoop/")
     hdfs_location = os.path.join(hadoop_path, "bin", "hdfs")
     process = subprocess.Popen(hdfs_location + " getconf -confKey " + attribute, stdout=subprocess.PIPE, shell=True)
     paths, _ = process.communicate()
-    paths = paths.split(",")
 
-    for i in range(0, len(paths)):
-        paths[i] = paths[i].strip()
-        if paths[i].startswith("file://"):
-            paths[i] = paths[i][7:]
+    return parse_paths(paths)
 
-    return paths
-
-def get_slaves(remove_host=False):
+def get_slaves(remove_host=True):
     hadoop_path = os.getenv("HADOOP_HOME", "/usr/local/hadoop/")
     slaves_file = os.path.join(hadoop_path, "etc", "hadoop", "slaves")
     slaves = []
