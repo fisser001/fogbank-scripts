eecc87e285dd82de9e1e5ae2a9a81549e1f8c080
libunamari <marialibunao@gmail.com>
2017-10-31 14:09:36 +1300

    Change path to reflect repository name

--- start_monitor.py
+++ start_monitor.py
@@ -7,9 +7,9 @@ import paramiko
 from get_hadoop_attributes import get_slaves
 
 def start_monitor(slave_name):
-    cmd = 'scp /home/hduser/cluster/cpu_ram_monitor.py {}:~/'.format(slave_name)
+    cmd = 'scp ~/fogbank-scripts/cpu_ram_monitor.py {}:~/'.format(slave_name)
     os.system(cmd)
-    cmd = 'scp /home/hduser/cluster/get_hadoop_attributes.py {}:~/'.format(slave_name)
+    cmd = 'scp ~/fogbank-scripts/get_hadoop_attributes.py {}:~/'.format(slave_name)
     os.system(cmd)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -23,7 +23,7 @@ def start_monitor(slave_name):
 cmd = 'pkill -9 -f cpu_ram_monitor_main.py'
 os.system(cmd)
 time.sleep(0.5)
-cmd =  'export HTTP_PROXY=""; export HTTPS_PROXY=""; export http_proxy=""; export https_proxy=""; nohup python /home/hduser/cluster/cpu_ram_monitor_main.py > /home/hduser/why.txt 2>&1&'
+cmd =  'export HTTP_PROXY=""; export HTTPS_PROXY=""; export http_proxy=""; export https_proxy=""; nohup python ~/fogbank-scripts/cpu_ram_monitor_main.py > /dev/null 2>&1&'
 os.system(cmd)
 
 for slave in get_slaves():
